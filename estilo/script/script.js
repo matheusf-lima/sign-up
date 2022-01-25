02e51ad5d1887d6493a155abda4256c8514b9946
matheuslima <97364500+matheusf-lima@users.noreply.github.com>
2022-01-25 20:33:20 -0300

    criando crud e cadastrando usuario pelo form

--- estilo/script/script.js
+++ estilo/script/script.js
@@ -1,33 +1,88 @@
+//Função para validação e login
+
 let user = document.getElementById('txtuser');
 let senha = document.getElementById('txtsenha');
 let erro = document.getElementById('txterro')
+let loginform = document.getElementById('formlogin')
+
 
 function logar(){
-  if(user.value.length == 0 || senha.value.length == 0){
-    erro.innerHTML = 'Preencha todos os campos para continuar'
-  } else if (user.value == 'admin' && senha.value == 'admin'){
-      window.alert("Bem vindo ademiro")
-      erro.innerHTML = ''
-      window.location = 'home.html'
-      return
-    } else {
-      erro.innerHTML = 'Usuario ou senha Invalidos'
+  if(loginform.reportValidity()){
+    const consultar = readClient()
+    if(user.value == consultar.nome) {
+      erro.innerHTML = 'achei o boi'
     }
-  
-  
+  } else [
+    erro.innerHTML = 'fail'
+  ]
 }
+// --------------------------------------------------
 
-
+//Função para pegar os itens e voltar pro login
 let novouser = document.getElementById('txtnovouser')
 let novasenha = document.getElementById('txtnovasenha')
 let confirmsenha = document.getElementById('txtconfirmsenha')
+let form = document.getElementById('formsign')
+
+const isvalidFilds = () => {
+  if(form.reportValidity() && novasenha.value == confirmsenha.value) {
+    return true
+  } else if(novasenha.value != confirmsenha.value) {
+    erro.innerHTML = 'As senhas são diferentes!'
+    return false
+  } 
+    //retorna true se todos os campos estiverem preenchidos e as senhas iguais
+}
 
 
-function cadastrar(){
-  if(novouser.value.length == 0 || novasenha.value.length == 0 || confirmsenha.value.length == 0){
-    erro.innerHTML = 'Preencha todos os campos para continuar'
-  } else{
-    erro.innerHTML = 'Cadastrado(a) com sucesso!'
+const saveClient = () => {
+  if (isvalidFilds()) {
+    const clinet = {
+      nome: novouser.value,
+      senha: novasenha.value
+    }
+    createClient(clinet)
     window.location = 'index.html'
+    erro.innerText = 'Cadastrado com sucesso!'
   }
 }
+
+
+
+//crud no localstorage
+
+const tempClient = {
+  nome: "Lucas",
+  senha: "novasenha"
+} 
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_usuario')) ?? []
+
+const setLocalStorage = (dbClient) => localStorage.setItem('db_usuario',JSON.stringify(dbClient))
+
+
+//create
+const createClient = (client) => {
+  const dbClient = getLocalStorage()
+  dbClient.push(client)
+  setLocalStorage(dbClient)
+}
+
+//read
+const readClient = () => getLocalStorage()
+
+//update
+const updateClient = (index, client) => {
+  const dbClient = readClient()
+  dbClient[index] = client
+  setLocalStorage(dbClient)
+}
+
+//delete
+
+const deleteClient = (index) => {
+  const dbClient = readClient()
+  dbClient.splice(index, 1)
+  setLocalStorage(dbClient)
+}
+
