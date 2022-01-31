a7862c81ab21f5b697d66ce10bfc838ad8ad47fc
matheuslima <97364500+matheusf-lima@users.noreply.github.com>
2022-01-30 21:30:32 -0300

    menu bar funcionando com icon

--- estilo/script/script2.js
+++ estilo/script/script2.js
@@ -1,10 +1,11 @@
-
-
 document.getElementById('menu-toggle').addEventListener('click', () => {
     document.getElementById('menu-toggle').classList.toggle('active')
     document.querySelector('nav').classList.toggle('active')
 })
 
+document.querySelector('li#a').addEventListener('mouseenter', () =>{
+    document.querySelector('a').classList.toggle('active')
+})
 
 
 
